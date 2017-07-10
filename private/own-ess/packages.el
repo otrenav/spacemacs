706043c52a83b1777ee66bb7cc7b6fb84b25e9d0
Omar Trejo <otrenav@gmail.com>
2017-07-09 20:52:31 -0500

    Avoid aligning comments towards the right in R

--- private/own-ess/packages.el
+++ private/own-ess/packages.el
@@ -4,9 +4,9 @@
 
 (defun own-ess/post-init-ess ()
   (setq-default ess-language "R")
-  (setq-default ess-fancy-comments nil)
   (setq-default inferior-R-program-name "R")
   (setq-default ess-ask-for-ess-directory nil)
+  (setq-default ess-indent-with-fancy-comments nil)
   (setq-default flycheck-lintr-linters
                 (concat "with_defaults(line_length_linter(80), "
                         ;; "absolute_paths_linter = NULL, "
