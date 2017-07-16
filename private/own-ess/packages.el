b7795cf2a6f3068b03fd6cc3b161fb567636c67e
Omar Trejo <otrenav@gmail.com>
2017-07-16 18:11:04 -0500

    Add ESS mode hook for R configuration

--- private/own-ess/packages.el
+++ private/own-ess/packages.el
@@ -2,19 +2,22 @@
 (defconst own-ess-packages
   '(ess))
 
-(defun own-ess/post-init-ess ()
-  (setq-default ess-language "R")
-  (setq-default ess-indent-level 4)
-  (setq-default ess-indent-offset 4)
-  (setq-default ess-default-style 'OWN)
-  (setq-default ess-toggle-underscore nil)
-  (setq-default inferior-R-program-name "R")
-  (setq-default ess-ask-for-ess-directory nil)
-  (setq-default ess-indent-with-fancy-comments nil)
-  (setq-default flycheck-lintr-linters
-                (concat "with_defaults(line_length_linter(80), "
-                        ;; "absolute_paths_linter = NULL, "
-                        ;; "camel_case_linter = NULL, "
-                        ;; "snake_case_linter = NULL, "
-                        "trailing_blank_lines_linter = NULL, "
-                        "commented_code_linter = NULL)")))
+(defun own-ess/pre-init-ess ()
+  (add-hook
+   'ess-mode-hook
+   (lambda()
+     (setq ess-language "R")
+     (setq ess-indent-level 4)
+     (setq ess-indent-offset 4)
+     (setq ess-default-style 'OWN)
+     (setq ess-toggle-underscore nil)
+     (setq inferior-R-program-name "R")
+     (setq ess-ask-for-ess-directory nil)
+     (setq ess-indent-with-fancy-comments nil)
+     (setq flycheck-lintr-linters
+           (concat "with_defaults(line_length_linter(80), "
+                   ;; "absolute_paths_linter = NULL, "
+                   ;; "camel_case_linter = NULL, "
+                   ;; "snake_case_linter = NULL, "
+                   "trailing_blank_lines_linter = NULL, "
+                   "commented_code_linter = NULL)")))))
