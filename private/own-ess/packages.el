ffb1f3a96bb092eb2bed8592ee8d076ed3c7dabc
Omar Trejo <otrenav@gmail.com>
2017-07-16 19:04:25 -0500

    Add R setup to REPL also

--- private/own-ess/packages.el
+++ private/own-ess/packages.el
@@ -3,21 +3,21 @@
   '(ess))
 
 (defun own-ess/pre-init-ess ()
-  (add-hook
-   'ess-mode-hook
-   (lambda()
-     (setq ess-language "R")
-     (setq ess-indent-level 4)
-     (setq ess-indent-offset 4)
-     (setq ess-default-style 'OWN)
-     (setq ess-toggle-underscore nil)
-     (setq inferior-R-program-name "R")
-     (setq ess-ask-for-ess-directory nil)
-     (setq ess-indent-with-fancy-comments nil)
-     (setq flycheck-lintr-linters
-           (concat "with_defaults(line_length_linter(80), "
-                   ;; "absolute_paths_linter = NULL, "
-                   ;; "camel_case_linter = NULL, "
-                   ;; "snake_case_linter = NULL, "
-                   "trailing_blank_lines_linter = NULL, "
-                   "commented_code_linter = NULL)")))))
+  (defun r-setup ()
+    (setq ess-language "R")
+    (setq ess-indent-level 4)
+    (setq ess-indent-offset 4)
+    (setq ess-default-style 'OWN)
+    (setq ess-toggle-underscore nil)
+    (setq inferior-R-program-name "R")
+    (setq ess-ask-for-ess-directory nil)
+    (setq ess-indent-with-fancy-comments nil)
+    (setq flycheck-lintr-linters
+          (concat "with_defaults(line_length_linter(80), "
+                  ;; "absolute_paths_linter = NULL, "
+                  ;; "camel_case_linter = NULL, "
+                  ;; "snake_case_linter = NULL, "
+                  "trailing_blank_lines_linter = NULL, "
+                  "commented_code_linter = NULL)")))
+  (add-hook 'ess-mode-hook 'r-setup)
+  (add-hook 'inferior-ess-mode-hook 'r-setup))
