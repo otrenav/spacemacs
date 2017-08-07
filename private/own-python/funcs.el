c7d4910d356513aa44d0d685848f151f88a9172b
Omar Trejo <otrenav@gmail.com>
2017-08-06 21:47:12 -0500

    Django shell for CVEST

--- private/own-python/funcs.el
+++ private/own-python/funcs.el
@@ -1,12 +1,24 @@
 
+(setenv "WORKON_HOME" "~/Projects/system/python/envs/")
+
 (defun own/python/score-data-django-shell ()
   (interactive)
-
-  ;; TODO: This may not work:
-  (venv-workon "score-data")
-
+  (pyvenv-workon "score-data")
   (setq python-shell-interpreter-args
         (concat "-i /home/otrenav/Projects/datata/score-data/"
+                "project/manage.py shell_plus"))
+  (run-python)
+  (split-window-right)
+  (other-window 1)
+  (switch-to-buffer (get-buffer-create "*Python*"))
+  (setq python-shell-interpreter-args "-i"))
+
+
+(defun own/python/cvest-django-shell ()
+  (interactive)
+  (pyvenv-workon "cvest")
+  (setq python-shell-interpreter-args
+        (concat "-i /home/otrenav/Projects/personal/cvest/"
                 "project/manage.py shell_plus"))
   (run-python)
   (split-window-right)
