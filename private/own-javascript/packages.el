3e6ee995790de075c078d7702812091340dffd71
otrenav <omar@otrenav.com>
2018-08-30 01:37:22 -0500

    Remove unncessary code

--- private/own-javascript/packages.el
+++ private/own-javascript/packages.el
@@ -1,13 +1,14 @@
+;;
+;; Setup:
+;; $ sudo npm install -g prettier
+;;
 
 (defconst own-javascript-packages
   '(prettier-js))
-
-(message "INSIDE 1 ************************")
 
 (defun own-javascript/init-prettier-js ())
 
 (defun own-javascript/pre-init-prettier-js ()
   (require 'prettier-js)
-  (message "INSIDE 2 ************************")
   (add-hook 'js2-mode-hook 'prettier-js-mode)
   (add-hook 'web-mode-hook 'prettier-js-mode))
